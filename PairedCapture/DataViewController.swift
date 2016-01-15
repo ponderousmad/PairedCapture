267d7e1a1e747b7fee4d92b324a847841ecdd7d3
Adrian Smith <agnomen@gmail.com>
2016-01-15 10:12:22 -0500

    Initial Commit

---
+++ PairedCapture/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  PairedCapture
+//
+//  Created by Adrian Smith on 2016-01-15.
+//  Copyright © 2016 Adrian Smith. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
