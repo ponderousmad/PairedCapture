7c569987f878083ca5743bc9799bf4ad740cc7c1
Adrian Smith <agnomen@gmail.com>
2016-01-16 21:30:37 -0500

    Add a view controller to show structure output

---
+++ PairedCapture/CaptureViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CaptureViewController.swift
+//  PairedCapture
+//
+//  Created by Adrian Smith on 2016-01-16.
+//  Copyright © 2016 Adrian Smith. All rights reserved.
+//
+
+import Foundation
+
+class CaptureViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }    
+}
